601e61a6cd920ee0446d83634b200e9c1aaad8cf
Victor Lagoon <victorlagoon@victors-imac.lan>
2023-01-11 14:35:44 -0500

    Initial Commit

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeModule } from './home/home.module';
+import { CoreModule } from './core/core.module';
+import { ErrorInterceptor } from './core/interceptors/error.interceptor';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { LoadingInterceptor } from './core/interceptors/loading.intercepter';
+import { JwtInterceptor } from './core/interceptors/jwt.interceptor';
+
+import { OrdersComponent } from './account/orders/orders.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    HomeModule,
+    CoreModule,
+    NgxSpinnerModule
+  ],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true}
+  ],
+  bootstrap: [AppComponent]
+})
+
+export class AppModule{ }
+
+
+
---
+++ client/src/app/checkout/checkout-payment/checkout-payment.component.ts
@@ -0,0 +1,104 @@
+import { AfterViewInit, Component, ElementRef, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { NavigationExtras, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { BasketService } from 'src/app/basket/basket.service';
+import { IBasket } from 'src/app/shared/models/basket';
+import { IOrder } from 'src/app/shared/models/order';
+import { CheckoutService } from '../checkout.service';
+
+declare var Stripe;
+
+@Component({
+  selector: 'app-checkout-payment',
+  templateUrl: './checkout-payment.component.html',
+  styleUrls: ['./checkout-payment.component.scss']
+})
+export class CheckoutPaymentComponent implements AfterViewInit, OnDestroy {
+  @Input() checkoutForm: FormGroup;
+  @ViewChild('cardNumber', {static: true}) cardNumberElement: ElementRef;
+  @ViewChild('cardExpiry', {static: true}) cardExpiryElement: ElementRef;
+  @ViewChild('cardCvc', {static: true}) cardCvcElement: ElementRef;
+  stripe: any;
+  cardNumber: any;
+  cardExpiry: any;
+  cardCvc: any;
+  cardErrors: any;
+  cardHandler = this.onChange.bind(this);
+
+  constructor(
+    private basketService: BasketService,
+    private checkoutService: CheckoutService,
+    private toastr: ToastrService, 
+    private router: Router) { }
+
+  ngAfterViewInit(): void {
+    this.stripe = Stripe('pk_test_51K0uBTK9EMg2SbXG0FsdK62f9K12D8BFjWwAY9w5fdWnqRd8EjgqWzUAGB12NjP898qH8r5rmArv3b8BsiGk0Aqy00eBZdi65R');
+    const elements = this.stripe.elements();
+
+    this.cardNumber = elements.create('cardNumber');
+    this.cardNumber.mount(this.cardNumberElement.nativeElement);
+    this.cardNumber.addEventListener('change', this.cardHandler);
+
+    this.cardExpiry = elements.create('cardExpiry');
+    this.cardExpiry.mount(this.cardExpiryElement.nativeElement);
+    this.cardExpiry.addEventListener('change', this.cardHandler);
+
+    this.cardCvc = elements.create('cardCvc');
+    this.cardCvc.mount(this.cardCvcElement.nativeElement);
+    this.cardCvc.addEventListener('change', this.cardHandler);
+    
+    
+  }   
+  
+  ngOnDestroy(): void {
+      this.cardNumber.destory();
+      this.cardExpiry.destory();
+      this.cardCvc.destory();
+  }
+
+  onChange({error}){
+    if (error){
+      this.cardErrors = error.message;
+    }else {
+      this.cardErrors = null;
+    }
+  }
+
+  submitOrder() {
+    const basket = this.basketService.getCurrentBasketValue();
+    const orderToCreate = this.getOrderToCreate(basket);
+    this.checkoutService.createOrder(orderToCreate).subscribe((order: IOrder) => {
+      this.toastr.success('Order created sucessfully');
+      this.stripe.confirmCardPayment(basket.clientSecret, {
+        payment_method: {
+          card: this.cardNumber,
+          billing_details: {
+            name: this.checkoutForm.get('paymentForm').get('nameOnCard').value 
+          }
+        }
+      }).then(result => {
+        console.log(result);
+        if (result.paymentIntent) {
+          this.basketService.deleteLocalBasket(basket.id);
+          const navigationExtras: NavigationExtras = {state: order};
+          this.router.navigate(['checkout/success'], navigationExtras);
+        } else{
+          this.toastr.error('Payment error');
+        }
+      });
+    }, error => {
+      this.toastr.error(error.message);
+      console.log(error);
+    });
+  }
+
+   private getOrderToCreate(basket: IBasket) {
+    return {
+      basketId: basket.id,
+      deliverMethodId: +this.checkoutForm.get('deliveryForm').get('deliveryMethod').value,
+      shipToAddress: this.checkoutForm.get('addressForm').value
+    };
+  }
+
+}
